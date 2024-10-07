106c60e647b0945073587393593c75583a0e84ec
Dhaval Bera <108049524+Dhaval369@users.noreply.github.com>
2024-10-07 11:05:21 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,21 +3,26 @@
 
 import PackageDescription
 
+let remoteUrl = "https://github.com/Dhaval369/SPMDemo/raw/main/SnapSPMDemo/Archive/SCSDKCameraKit.xcframework.zip"
+let remoteChecksum = "38041cb81459503e4e304c091f9a2df12f3c788862de6db704877ac1342aa08b"
+let packageName = "MyLibrary"
+
 let package = Package(
     name: "MyLibrary",
     defaultLocalization: "en", platforms: [.iOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "MyLibrary",
-            targets: ["MyLibrary"]),
+            name: packageName,
+            targets: [packageName]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .binaryTarget(name: "SCSDKCameraKit", path: "./MyLibrary/Frameworks/SCSDKCameraKit.xcframework"),
-        .binaryTarget(name: "SCSDKCoreKit", path: "./MyLibrary/Frameworks/SCSDKCoreKit.xcframework"),
-        .binaryTarget(name: "SCSDKCreativeKit", path: "./MyLibrary/Frameworks/SCSDKCreativeKit.xcframework"),
-        .target(name: "MyLibrary", dependencies: ["SCSDKCameraKit", "SCSDKCoreKit", "SCSDKCreativeKit"], path: "./Sources")
+        //.binaryTarget(name: "SCSDKCameraKit", path: "./MyLibrary/Frameworks/SCSDKCameraKit.xcframework"),
+        //.binaryTarget(name: "SCSDKCoreKit", path: "./MyLibrary/Frameworks/SCSDKCoreKit.xcframework"),
+        //.binaryTarget(name: "SCSDKCreativeKit", path: "./MyLibrary/Frameworks/SCSDKCreativeKit.xcframework"),
+        .binaryTarget(name: "SCSDKCameraKit", url: remoteUrl, checksum: remoteChecksum),
+        .target(name: packageName, dependencies: ["SCSDKCameraKit"], path: "./Sources")
     ]
 )
