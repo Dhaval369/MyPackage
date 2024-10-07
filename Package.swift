055d1544f8ae28f1b4b7e11c6271cb55367bd834
Dhaval Bera <108049524+Dhaval369@users.noreply.github.com>
2024-10-07 11:43:51 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,9 +3,20 @@
 
 import PackageDescription
 
+let packageName = "MyLibrary"
+
 let remoteUrl = "https://github.com/Dhaval369/SPMDemo/raw/main/SnapSPMDemo/Archive/SCSDKCameraKit.xcframework.zip"
 let remoteChecksum = "38041cb81459503e4e304c091f9a2df12f3c788862de6db704877ac1342aa08b"
-let packageName = "MyLibrary"
+let cameraKit = "SCSDKCameraKit"
+
+let coreKit = "SCSDKCoreKit"
+let coreKitURL = "https://github.com/Dhaval369/SPMDemo/raw/main/SnapSPMDemo/Archive/SCSDKCoreKit.xcframework.zip"
+let coreKitCheckSum = "071db45af3936f28423f557bef4b7caf27636742f1658c8f8c21021e5f7a2406"
+
+let creativeKit = "SCSDKCreativeKit"
+let creativeKitURL = "https://github.com/Dhaval369/SPMDemo/raw/main/SnapSPMDemo/Archive/SCSDKCreativeKit.xcframework.zip"
+let creativeKitCheckSum = "ecd706e8601c38d091552b64242b1419db7130fae45446a015450a3a4fffabef"
+
 
 let package = Package(
     name: "MyLibrary",
@@ -22,7 +33,9 @@ let package = Package(
         //.binaryTarget(name: "SCSDKCameraKit", path: "./MyLibrary/Frameworks/SCSDKCameraKit.xcframework"),
         //.binaryTarget(name: "SCSDKCoreKit", path: "./MyLibrary/Frameworks/SCSDKCoreKit.xcframework"),
         //.binaryTarget(name: "SCSDKCreativeKit", path: "./MyLibrary/Frameworks/SCSDKCreativeKit.xcframework"),
-        .binaryTarget(name: "SCSDKCameraKit", url: remoteUrl, checksum: remoteChecksum),
-        .target(name: packageName, dependencies: ["SCSDKCameraKit"], path: "./MyLibrary/Sources")
+        .binaryTarget(name: creativeKit, url: creativeKitURL, checksum: creativeKitCheckSum),
+        .binaryTarget(name: coreKit, url: coreKitURL, checksum: coreKitCheckSum),
+        .binaryTarget(name: cameraKit, url: remoteUrl, checksum: remoteChecksum),
+        .target(name: packageName, dependencies: [cameraKit, coreKit, creativeKit], path: "./MyLibrary/Sources")
     ]
 )
